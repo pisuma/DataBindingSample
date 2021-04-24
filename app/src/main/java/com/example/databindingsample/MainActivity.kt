0096b31e7dc5d7218739e5f086a56452b8369e72
pisuma <satojota@MacBook-Pro-2.local>
2021-04-24 09:23:12 +0900

    DataBindingセットアップとレイアウトとActivityの紐付けをしてfindViewByIdを使わずにidを取得できるように実装

--- app/src/main/java/com/example/databindingsample/MainActivity.kt
+++ app/src/main/java/com/example/databindingsample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.databindingsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.databindingsample.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.sampleButton.setOnClickListener {
+            binding.sampleTextView.text = "Hello, Binding!"
+
+        }
+
+
+//        val sampleTextView: TextView = findViewById(R.id.sample_text_view)
+//        sampleTextView.text = "Hello Android"
+
+
     }
 }
