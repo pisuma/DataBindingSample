16a965ed98a82359d1fa58c2f3ee9b8c00bb1eb7
pisuma <sakigakepisumar@gmail.com>
2021-04-24 11:01:19 +0900

    双方向データバインディング実装

--- app/src/main/java/com/example/databindingsample/MainActivity.kt
+++ app/src/main/java/com/example/databindingsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.databindingsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.databindingsample.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,9 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.counter += 1
-        binding.sampleButton.setOnClickListener {
-            binding.counter += 1
+
+        binding.buttonConfirm.setOnClickListener {
+            Toast.makeText(this, binding.inputText, Toast.LENGTH_LONG).show()
         }
 
 
